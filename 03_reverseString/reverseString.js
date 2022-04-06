89215cdbfe179a1cb8a4bdfafd98c3416bfdb081
Alex <kuczera.alexander@gmail.com>
2022-04-05 18:15:51 -0600

    My solution to reverseString exercise using array methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	string = string.split('');
+  let output = '';
+ 	
+  for (i = string.length - 1; i >= 0; i--) {
+  	output += string[i];
+  }
+  
+	return(output);
 };
 
 // Do not edit below this line
