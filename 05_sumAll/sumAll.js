8bc8087243b0350bfc47983d660942dfa8cd8acd
Alex <kuczera.alexander@gmail.com>
2022-04-06 17:51:31 -0600

    solution for sumAll exercise.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(one, two) {
+	let sum = 0;
+  
+  if (one < 0 || two < 0 || !Number.isInteger(one) || !Number.isInteger(two)) {
+  return "ERROR";
+  } else if (one > two) {
+  
+  for (i = two; i <= one; i++) 
+  sum = sum + i;
+  
+  } else {
+  
+  for (i = one; i <= two; i++) 
+  sum = sum + i;
+  
+  } 
+  return sum;
 };
 
 // Do not edit below this line
