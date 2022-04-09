8b0d8e0db821820b8c1ac44dcede9939b4559a51
Alex <kuczera.alexander@gmail.com>
2022-04-09 10:11:35 -0600

    Solution to tempConversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  celsius = ((fahrenheit - 32) * (5 / 9)).toFixed(1);
+  return parseFloat(celsius);
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+  fahrenheit = ((celsius * ( 9 / 5 )) + 32).toFixed(1);
+  return parseFloat(fahrenheit);
 };
 
 // Do not edit below this line
