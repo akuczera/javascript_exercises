125ff186e252499a1a63083a88ad0174e12b3cf0
Alex <kuczera.alexander@gmail.com>
2022-04-09 09:27:17 -0600

    Solution to leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+	if (year % 100 == 0 && year % 4 == 0 && year % 400 == 0) {
+  	console.log("TRUE");
+  	return true;
+  } else if (year % 100 == 0 && year % 4 == 0) {
+  	console.log("FALSE");
+  	return false;
+  } else if (year % 4 == 0) {
+  	console.log("TRUE");
+  	return true;
+  } else {
+  	console.log("FALSE");
+  	return false;
+  }
 };
 
+//Not as clean as the solution, but I went with what I knew here at the time.
 // Do not edit below this line
 module.exports = leapYears;
