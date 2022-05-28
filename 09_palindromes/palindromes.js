86d172f51f36040fe76caa52dfb42df8eebe5d55
Alex <kuczera.alexander@gmail.com>
2022-05-28 17:09:04 -0600

    Complete palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //take in the string, remove all punctuation and put into an array, then convert back to string
+    let letterArray = string.toLowerCase().split('').filter(el => el.match(/[A-Z]/i)).join('');
+    //flip the array in reverse in another variable, and end up with reversed string
+    let reversed = string.toLowerCase().split('').filter(el => el.match(/[A-Z]/i)).reverse().join('');
+    //compare the two strings if they're the exact same
+        //then return true.
+        //else, return false
+    	        if(letterArray === reversed) {
+    	        return true;
+    	        } else {
+    	        return false;
+    	        }
 
-};
+}
 
 // Do not edit below this line
 module.exports = palindromes;
