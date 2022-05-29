2c5920c4a99a600a76fd84982db3d77bd64fe8c5
Alex <kuczera.alexander@gmail.com>
2022-05-29 11:20:41 -0600

    Completed fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const int = parseInt(num);
+    let array = [1, 1];
 
+    if(int < 0) {
+        return "OOPS";
+    }else if(int == 0 || int == 1){
+        return 1;
+    }else {
+        for (let i = 2; i <= int; i++) {
+            let sum = array[i-1] + array[i-2];
+            array.push(sum);
+        }
+        return array[int-1];
+
+    }
 };
 
 // Do not edit below this line
