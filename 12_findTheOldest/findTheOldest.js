3bd65bd2a6d820963bde1d8b6c6b4dd8326b3f17
Alex <kuczera.alexander@gmail.com>
2022-05-29 14:32:52 -0600

    Completed getTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+	const sorted = array.sort(function(a, b){
+  	if(!a.yearOfDeath) {
+    	var first = (new Date().getFullYear()) - a.yearOfBirth;
+      }else {
+      	var first = (a.yearOfDeath - a.yearOfBirth);
+      }
 
-};
+    if(!b.yearOfDeath) {
+    	var second = (new Date().getFullYear()) - b.yearOfBirth;
+      }else {
+      	var second = (b.yearOfDeath - b.yearOfBirth);
+      }
+
+      if(first > second) {
+      return -1;
+      } else {
+      return 1;
+      }
+});
+return sorted[0];
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
